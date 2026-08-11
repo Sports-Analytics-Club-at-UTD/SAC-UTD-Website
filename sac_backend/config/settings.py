b28ea8af4de67c141e1c238fc75c10343a2a4d67
Rishi Boggavarapu <83251988+TheFriendlyStudent@users.noreply.github.com>
2026-08-11 18:24:38 -0400

    Add localhost:5173 to CORS allowed origins

--- sac_backend/config/settings.py
+++ sac_backend/config/settings.py
@@ -156,6 +156,7 @@ CORS_ALLOWED_ORIGINS = env.list(
     "CORS_ALLOWED_ORIGINS",
     default=[
         "http://localhost:3000",
+        "http://localhost:5173",
         "http://127.0.0.1:3000",
     ],
 )
