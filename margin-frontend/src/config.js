1c732e5dd25e64d9841da34d3e747d75cddb50e4
Rishi Boggavarapu <83251988+TheFriendlyStudent@users.noreply.github.com>
2026-08-26 15:25:47 -0500

    typo

--- margin-frontend/src/config.js
+++ margin-frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://sac-utd-website.onrender.com/';
+export const BASE_URL = 'https://sac-utd-website.onrender.com';
 export const API_BASE_URL = "https://sac-utd-website.onrender.com/api";
